b053d10b85491746bdfe9adcd1a7eafe984eb7d4
soulwawa <soulwawa85@gmail.com>
2018-10-03 17:47:10 +0900

    mysite basic

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,8 @@ from blog import views
 app_name = 'blog'
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('project/', views.project, name='project'),
+    path('devlog/', views.devlog, name='devlog')
 ]
--- blog/views.py
+++ blog/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "base.html")
+
+
+def about(request):
+    return render(request, "about.html")
+
+
+def project(request):
+    return render(request, "project.html")
+
+
+def devlog(request):
+    return render(request, "devlog.html")
