844b2e31a538134a05baf45955ce62447d25faf0
soulwawa <soulwawa85@gmail.com>
2018-10-15 00:39:07 +0900

    deploy setting #13

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin-soulwawa/', admin.site.urls),
     path('martor/', include('martor.urls')),
     path('', include("blog.urls", namespace='blog')),
 
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
+              + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
