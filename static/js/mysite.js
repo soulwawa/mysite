5f23cd0776b0d7293f87db57340208d350d76736
soulwawa <soulwawa85@gmail.com>
2018-10-22 00:56:19 +0900

    go to top add

--- static/js/mysite.js
+++ static/js/mysite.js
@@ -1,7 +1,13 @@
 $(window).scroll(function() {
   if($(window).scrollTop() + $(window).height() > $(document).height() - 100) {
-    $('.scroll-container').css({'display':'none'})
+    $('.scroll-container').css({'display':'none'});
+    $('#myBtnTop').css({'display':'block'});
   }else {
-    $('.scroll-container').css({'display':'block'})
+    $('.scroll-container').css({'display':'block'});
+    $('#myBtnTop').css({'display':'none'});
   }
 });
+function goTopFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
