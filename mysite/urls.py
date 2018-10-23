35cdcd35aec3addf02d93ee6ef89e992dda1025e
soulwawa <soulwawa85@gmail.com>
2018-10-24 00:13:47 +0900

    add robots.txt, google AdSense

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin-soulwawa/', admin.site.urls),
     path('martor/', include('martor.urls')),
-    path('', include("blog.urls", namespace='blog'))
+    path('', include("blog.urls", namespace='blog')),
+    path('robots.txt/', TemplateView.as_view(template_name="robots.txt", content_type="text/plain"))
 ]
 
 if settings.DEBUG:
