7dd74b22b7862d03682c373c1cc769af20dac4f6
soulwawa <soulwawa85@gmail.com>
2018-10-29 00:40:41 +0900

    url update
    attacker defense

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,7 @@
-from django.urls import path
+from django.urls import path, re_path
+from django.shortcuts import redirect
 from blog import views
+
 
 app_name = 'blog'
 
@@ -10,7 +12,10 @@ urlpatterns = [
     path('dev-notes/', views.dev_notes, name='dev-notes'),
     path('dev-notes/search', views.dev_search, name='dev-search'),
     path('dev-notes/detail/<title>', views.dev_detail, name='dev-detail'),
-    path('dev-notes/<tag>/', views.tag_search, name='tag-search')
+    path('dev-notes/<tag>/', views.tag_search, name='tag-search'),
 
+    # attacker redirect
+    path('wp-content/', lambda request: redirect('blog:dev-notes')),
+    re_path(r'^.*\.php/$', lambda request: redirect('blog:dev-notes')),
     # path('test/', views.test)
 ]
