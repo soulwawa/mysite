9b9a0eb78098f22eee69fadf9d608a31372928a1
soulwawa <soulwawa85@gmail.com>
2018-12-03 00:45:24 +0900

    project update
    project update
    search font remove

--- blog/data/project.py
+++ blog/data/project.py
@@ -3,6 +3,14 @@ project_data = [
         "title": "Project - JOB",
         "image": "image/noun_Accounts Project_1875474.png",
         "Data": {
+            "TITLE": "Malinfo Web Service",
+            "Position": "All-round",
+            "Skill": "Python-Django, MongoDB, Neo4j, PostgreSQL",
+            "Detail": "Webpage"}},
+    {
+        "title": "Project - JOB",
+        "image": "image/noun_Accounts Project_1875474.png",
+        "Data": {
             "TITLE": "Cyber Threat Intelligence Collection System",
             "Position": "Python Developer",
             "Skill": "Python, Django, MongoDB",
