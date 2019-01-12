0f8700caf720136c9ecfbcae42e0a32523a438eb
soulwawa <soulwawa85@gmail.com>
2019-01-12 21:56:57 +0900

    [ADD] About page layout
    resume, about page add, layout settings
    footer fix

---
+++ static/js/mysite.js
@@ -0,0 +1,8 @@
+
+$('section').css({ 'height': $(window).height() });
+$(window).on('resize', function() {
+
+  // $('header').css({ 'height': $(window).height() });
+  $('body').css({ 'width': $(window).width() });
+  $('section').css({ 'height': $(window).height() });
+});
